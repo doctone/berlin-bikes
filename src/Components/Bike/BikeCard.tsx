fd66128c3a82fe690799d317aee774d5a5b81cbd
doctone <samjojames@gmail.com>
2022-04-15 21:58:41 +0100

    reduce info on bike cards

--- src/Components/Bike/BikeCard.tsx
+++ src/Components/Bike/BikeCard.tsx
@@ -4,16 +4,12 @@ import { Bike } from '../../Api/Client';
 import "./BikeCard.css";
 
 export function BikeCard({bike}:{bike:Bike}):JSX.Element {
-    const date = new Date(bike.date_stolen).toLocaleDateString();
     return (
         <div className='bike-card'>
             <img src={bike.large_img} alt={bike.description} width="200em" />
             <Link to={`./bikes/${bike.id}`}><h3> Stolen {bike.title}</h3></Link>
-            <h2>{bike.location_found}</h2>
-            <p><strong>S{bike.frame_model}</strong></p>
             <p>{bike.description}</p>
-            <p>{bike.id}</p>
-            <p>{bike.year}</p>
+            <p>{bike.year} {bike.stolen_location}</p>
         </div>
     )
 }
