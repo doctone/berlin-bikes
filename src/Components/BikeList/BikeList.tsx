01d0f554b21ff5c2647f708723a47aaea031bdf1
doctone <samjojames@gmail.com>
2022-04-15 22:20:51 +0100

    Restyle bike card

--- src/Components/BikeList/BikeList.tsx
+++ src/Components/BikeList/BikeList.tsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Bike, getBikes } from '../../Api/Client';
 import { BikeCard } from '../Bike/BikeCard';
+import ReactPaginate from 'react-paginate';
+import "./BikeList.css";
 
 export function BikeList():JSX.Element {
     const [bikes, setBikes] = useState<Array<Bike>>([]);
 
     useEffect(() => {
         getBikes().then(setBikes);
-    })
+    }, [])
 
     if (bikes === undefined) {
         return <div>loading...</div>
     }
     return (
-        <ul>
+        <>
+        <p className='bike-list__total'>Total Bikes: <strong>{bikes.length}</strong></p>
+        <ul className='bike-list'>
             {bikes.map(b => (
                 <BikeCard bike={b} />
             ))}
         </ul>
+        </>
     )
 }
