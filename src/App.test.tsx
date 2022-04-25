0a4047e2cd46c4ab19a7d70cab63ded5ebb70d25
Sam James <samjojames@gmail.com>
2022-04-25 13:51:27 +0100

    Create Heroku repo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BikeListPage } from './Pages/BikeListPage/BikeListPage';
+import { BikeList } from './Components/BikeList/BikeList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+
+    screen.debug();
+  });
+  test('displays title', () => {
+    render (<App />);
+
+    expect(screen.getByText(/Berlin/)).toBeInTheDocument();
+  })
 });
